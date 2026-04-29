c6d8dc4ea6be94303d59346f1af74616171ed58b
Adin Rama Ariyanto Putra <135962858+adinrama@users.noreply.github.com>
2026-04-29 08:28:21 +0700

    Delete frontend directory

--- frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    BACKEND_URL: process.env.BACKEND_URL || "http://localhost:5000",
-  },
-};
-
-module.exports = nextConfig;
